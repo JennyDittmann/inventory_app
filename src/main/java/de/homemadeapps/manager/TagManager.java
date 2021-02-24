8b19b7e570f19f6f6afdfedff400fadcbd0f6de8
Jenny Dittmann <jenny-dittmann@gmx.de>
2021-02-24 17:33:01 +0100

    Add TagManagerTests and TagManager Dummy-Methods

--- src/main/java/de/homemadeapps/manager/TagManager.java
+++ src/main/java/de/homemadeapps/manager/TagManager.java
@@ -1,2 +1,24 @@
-package de.homemadeapps.Manager;public class TagManager {
+package de.homemadeapps.manager;
+
+import de.homemadeapps.databaseSchemas.Tag;
+import de.homemadeapps.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TagManager {
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagManager(final TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> searchTagsByNameAndDescription(String query) {
+        List<Tag> test = new ArrayList<>();
+        return test;
+    }
 }
--- src/test/java/de/homemadeapps/manager/TagManagerTest.java
+++ src/test/java/de/homemadeapps/manager/TagManagerTest.java
@@ -1,12 +1,59 @@
 package de.homemadeapps.manager;
 
+import de.homemadeapps.databaseSchemas.Tag;
+import de.homemadeapps.repositories.TagRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TagManagerTest {
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private TagManager tagManager;
+
+    @Before
+    public void setUp() {
+        List<Tag> mockedData = new ArrayList<>(Arrays.asList(new Tag(1, "Krieg der Sterne", "This is really cool Star" +
+                " Wars stuff!"), new Tag(2, "Star", "Stuff"), new Tag(3, "Dinge", "Awesome Stuff yey")));
+        tagRepository.saveAll(mockedData);
+    }
 
     @Test
-    public void getTagByIdTest(){
+    public void saveTag_NoDuplicateExist_success() {
 
+    }
+
+    @Test
+    public void saveTag_DuplicateExist_fail() {
+
+    }
+
+    @Test
+    public void searchTagsByNameAndDescription_OnHavingData_ReturnList() {
+        List<Tag> expectedData = new ArrayList<>(Arrays.asList(new Tag(1, "Krieg der Sterne", "This is really cool Star" +
+                " Wars stuff!"), new Tag(2, "Star", "Stuff")));
+
+        List<Tag> result = tagManager.searchTagsByNameAndDescription("Star");
+
+        assertEquals(expectedData, result);
+    }
+
+    @Test
+    public void searchTagsByNameAndDescription_IfNotExist_ReturnEmptyList() {
+        List<Tag> expectedData = new ArrayList<>();
+
+        List<Tag> result = tagManager.searchTagsByNameAndDescription("test");
+
+        assertEquals(expectedData, result);
     }
 
 }
