a19d58bd54b507fd53b0d15509bfc80521fad894
Jenny Dittmann <jenny-dittmann@gmx.de>
2021-03-18 00:55:44 +0100

    Change itemController controller anotation to restcontroller, add slash in mappings

--- src/main/java/de/homemadeapps/controller/ItemController.java
+++ src/main/java/de/homemadeapps/controller/ItemController.java
@@ -2,68 +2,67 @@ package de.homemadeapps.controller;
 
 import de.homemadeapps.manager.ItemManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class ItemController {
     private final ItemManager itemManager;
 
     @Autowired
-    public ItemController(final ItemManager itemManager){
+    public ItemController(final ItemManager itemManager) {
         this.itemManager = itemManager;
     }
 
-    @PostMapping(value = "addItem", produces = "application/json")
-    public Map<String,Integer> addItem(final String name, final String description) {
-        Map<String,Integer> response = new HashMap<>();
+    @PostMapping(value = "/addItem", produces = "application/json")
+    public Map<String, Integer> addItem(final String name, final String description) {
+        Map<String, Integer> response = new HashMap<>();
 
-        try{
-            if(itemManager.saveItem(name,description)){
+        try {
+            if (itemManager.saveItem(name, description)) {
                 response.put("responseCode", 201);
-            }else{
+            } else {
                 response.put("responseCode", 417);
             }
-        }catch (Exception e){
-            response.put("responseCode",500);
+        } catch (Exception e) {
+            response.put("responseCode", 500);
         }
 
         return response;
     }
 
-    @PostMapping(value = "updateItem", produces = "application/json")
+    @PostMapping(value = "/updateItem", produces = "application/json")
     public Map<String, Integer> updateItem(final int id, String name, final String description) {
-        Map<String,Integer> response = new HashMap<>();
+        Map<String, Integer> response = new HashMap<>();
 
-        try{
-            if(itemManager.updateItem(id, name,description)){
+        try {
+            if (itemManager.updateItem(id, name, description)) {
                 response.put("responseCode", 200);
-            }else{
+            } else {
                 response.put("responseCode", 417);
             }
-        }catch (Exception e){
-            response.put("responseCode",500);
+        } catch (Exception e) {
+            response.put("responseCode", 500);
         }
 
         return response;
     }
 
-    @PostMapping(value = "deleteItem", produces = "application/json")
-    public Map<String,Integer> deleteItem(final int id){
-        Map<String,Integer> response = new HashMap<>();
+    @PostMapping(value = "/deleteItem", produces = "application/json")
+    public Map<String, Integer> deleteItem(final int id) {
+        Map<String, Integer> response = new HashMap<>();
 
-        try{
-            if(itemManager.deleteItem(id)){
-                response.put("responseCode",200);
-            }else{
-                response.put("responseCode",417);
+        try {
+            if (itemManager.deleteItem(id)) {
+                response.put("responseCode", 200);
+            } else {
+                response.put("responseCode", 417);
             }
-        }catch (Exception e){
-            response.put("responseCode",500);
+        } catch (Exception e) {
+            response.put("responseCode", 500);
         }
 
         return response;
